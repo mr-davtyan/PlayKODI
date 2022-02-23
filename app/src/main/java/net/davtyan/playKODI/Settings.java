57da42af244e976df20f1fd56f50bb69ff979805
Artem D <mr.davtyan@gmail.com>
2022-02-23 14:27:47 -0500

    setting update

--- app/src/main/java/net/davtyan/playKODI/Settings.java
+++ app/src/main/java/net/davtyan/playKODI/Settings.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -184,7 +183,6 @@ public class Settings extends AppCompatActivity {
     }
 
     private void saveSettings() {
-        Log.w("save settings", "save settings");
         SharedPreferences.Editor editor = mSettings.edit();
 
         if (spinnerTheme.getSelectedItemPosition() == 0) {
