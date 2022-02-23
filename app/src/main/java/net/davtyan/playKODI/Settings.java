1ebd72dae15d3841b0a47f8e6e34c5ed332066f3
Artem D <mr.davtyan@gmail.com>
2022-02-23 13:29:36 -0500

    setting update

--- app/src/main/java/net/davtyan/playKODI/Settings.java
+++ app/src/main/java/net/davtyan/playKODI/Settings.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.Spinner;
@@ -43,24 +45,7 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
-
-        if (mSettings.getBoolean(APP_PREFERENCES_THEME_DARK_AUTO, true)) {
-            switch (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) {
-                case Configuration.UI_MODE_NIGHT_YES:
-                    setTheme(R.style.AppThemeDark);
-                    break;
-                case Configuration.UI_MODE_NIGHT_NO:
-                    setTheme(R.style.AppTheme);
-                    break;
-            }
-        } else {
-            if (mSettings.getBoolean(APP_PREFERENCES_THEME_DARK, true)) { //checking the theme
-                setTheme(R.style.AppTheme);
-            } else {
-                setTheme(R.style.AppThemeDark);
-            }
-        }
-
+        setTheme();
         setContentView(R.layout.settings);
 
         spinnerTheme = findViewById(R.id.spinnerTheme);
@@ -116,18 +101,19 @@ public class Settings extends AppCompatActivity {
                 spinnerDefaultHost.setSelection(0);
             }
 
-            if (checkBoxUseDefaultHost.isChecked()){
+            if (checkBoxUseDefaultHost.isChecked()) {
                 spinnerDefaultHost.setVisibility(View.VISIBLE);
                 findViewById(R.id.textViewDefaultHost).setVisibility(View.VISIBLE);
-            }else{
+            } else {
                 spinnerDefaultHost.setVisibility(View.INVISIBLE);
                 findViewById(R.id.textViewDefaultHost).setVisibility(View.INVISIBLE);
             }
             checkBoxUseDefaultHost.setOnCheckedChangeListener((arg0, isChecked) -> {
-                if (isChecked){
+                saveSettings();
+                if (isChecked) {
                     spinnerDefaultHost.setVisibility(View.VISIBLE);
                     findViewById(R.id.textViewDefaultHost).setVisibility(View.VISIBLE);
-                }else{
+                } else {
                     spinnerDefaultHost.setVisibility(View.INVISIBLE);
                     findViewById(R.id.textViewDefaultHost).setVisibility(View.INVISIBLE);
                 }
@@ -136,40 +122,86 @@ public class Settings extends AppCompatActivity {
             findViewById(R.id.layoutSpinner).setVisibility(View.GONE);
         }
 
+        checkBoxCopyToClipboard.setOnCheckedChangeListener((compoundButton, b) -> saveSettings());
+        checkBoxPreviewLink.setOnCheckedChangeListener((compoundButton, b) -> saveSettings());
+        spinnerDefaultHost.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                saveSettings();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+        int spinnerThemeCurrentSelected = spinnerTheme.getSelectedItemPosition();
+        spinnerTheme.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                saveSettings();
+                if (spinnerThemeCurrentSelected != i) {
+                    Intent intent = getIntent();
+                    finish();
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
+    }
+
+    private void setTheme() {
+        if (mSettings.getBoolean(APP_PREFERENCES_THEME_DARK_AUTO, true)) {
+            switch (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) {
+                case Configuration.UI_MODE_NIGHT_YES:
+                    setTheme(R.style.AppThemeDark);
+                    break;
+                case Configuration.UI_MODE_NIGHT_NO:
+                    setTheme(R.style.AppTheme);
+                    break;
+            }
+        } else {
+            if (mSettings.getBoolean(APP_PREFERENCES_THEME_DARK, true)) { //checking the theme
+                setTheme(R.style.AppTheme);
+            } else {
+                setTheme(R.style.AppThemeDark);
+            }
+        }
     }
 
     public void onClick(View v) {
-        Intent intentMyActivity = new Intent(Settings.this, MyActivity.class);
-
         int id = v.getId();
-        if (id == R.id.buttonSaveSettings) {  // save button
-            SharedPreferences.Editor editor = mSettings.edit();
-
-            if (spinnerTheme.getSelectedItemPosition() == 0){
-                editor.putBoolean(APP_PREFERENCES_THEME_DARK_AUTO, true);
-            } else if (spinnerTheme.getSelectedItemPosition() == 1){
-                editor.putBoolean(APP_PREFERENCES_THEME_DARK_AUTO, false);
-                editor.putBoolean(APP_PREFERENCES_THEME_DARK, false);
-            } else {
-                editor.putBoolean(APP_PREFERENCES_THEME_DARK_AUTO, false);
-                editor.putBoolean(APP_PREFERENCES_THEME_DARK, true);
-            }
-
-            editor.putBoolean(APP_PREFERENCES_COPY_LINKS, checkBoxCopyToClipboard.isChecked());
-            editor.putBoolean(APP_PREFERENCES_PREVIEW_LINKS, checkBoxPreviewLink.isChecked());
-            editor.putBoolean(APP_PREFERENCES_USE_DEFAULT_HOST, checkBoxUseDefaultHost.isChecked());
-            editor.putString(APP_PREFERENCES_DEFAULT_HOST, hostFullAddress.get(spinnerDefaultHost.getSelectedItemPosition()));
-            editor.apply();
-            startActivity(intentMyActivity);
-            finish();
-        } else if (id == R.id.buttonCloseSettings) {  // exit button
-            startActivity(intentMyActivity);
+        if (id == R.id.buttonCloseSettings) {  // exit button
+            startActivity(new Intent(Settings.this, MyActivity.class));
             finish();
         } else if (id == R.id.buttonEditHosts) {  // exit button
-            Intent intentEditHosts = new Intent(Settings.this, Hosts.class);
-            startActivity(intentEditHosts);
+            startActivity(new Intent(Settings.this, Hosts.class));
             finish();
         }
+    }
+
+    private void saveSettings() {
+        Log.w("save settings", "save settings");
+        SharedPreferences.Editor editor = mSettings.edit();
+
+        if (spinnerTheme.getSelectedItemPosition() == 0) {
+            editor.putBoolean(APP_PREFERENCES_THEME_DARK_AUTO, true);
+        } else if (spinnerTheme.getSelectedItemPosition() == 1) {
+            editor.putBoolean(APP_PREFERENCES_THEME_DARK_AUTO, false);
+            editor.putBoolean(APP_PREFERENCES_THEME_DARK, false);
+        } else {
+            editor.putBoolean(APP_PREFERENCES_THEME_DARK_AUTO, false);
+            editor.putBoolean(APP_PREFERENCES_THEME_DARK, true);
+        }
+
+        editor.putBoolean(APP_PREFERENCES_COPY_LINKS, checkBoxCopyToClipboard.isChecked());
+        editor.putBoolean(APP_PREFERENCES_PREVIEW_LINKS, checkBoxPreviewLink.isChecked());
+        editor.putBoolean(APP_PREFERENCES_USE_DEFAULT_HOST, checkBoxUseDefaultHost.isChecked());
+        editor.putString(APP_PREFERENCES_DEFAULT_HOST, hostFullAddress.get(spinnerDefaultHost.getSelectedItemPosition()));
+        editor.apply();
     }
 
     public void onBackPressed() { // Back button
